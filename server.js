d0f118f6d5221d14f59b42cde6e846dc1ef45caf
Ibrahim Abuaker <ibrahim.aaker@gmail.com>
2024-04-02 14:33:29 +0200

    completed server.js

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const connectDB = require("./dbinit");
 
 const PORT = process.env.PORT || 8090;
 
+app.use(cors())
+app.use(express.json())
+
 connectDB();
+
+app.get("/", (req, res)=> {
+    res.json("Welcome to my API")
+})
+
+app.listen(PORT, ()=> {
+    console.log("Listen on Port")
+})
